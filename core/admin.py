de38c63689932747c27cc73e4f16f891503a990a
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-02-15 13:53:26 +0700

    registering models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib.admin import site
+from .models import *
 
-# Register your models here.
+site.register(Item)
+site.register(OrderItem)
+site.register(Order)
+site.register(Coupon)
