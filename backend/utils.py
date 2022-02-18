4e82b5b0f7f4c2bc867b8d79fd5de05adf69f1a8
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-02-18 23:26:04 +0700

    login logout button

--- backend/utils.py
+++ backend/utils.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import user_passes_test
 
 def staff_required(view_func=None, redirect_=REDIRECT_FIELD_NAME, login_='admin:login'):
     actual_decorator = user_passes_test(
-        lambda u : u.is_active and u.is_staff,
+        lambda u: u.is_active and u.is_staff and u.is_superuser,
         login_url=login_,
         redirect_field_name=redirect_
     )
